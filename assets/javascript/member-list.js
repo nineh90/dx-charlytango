09f730e55ad38124a58f784e4ef1fc4d41cb2396
nineh90 <102158158+nineh90@users.noreply.github.com>
2022-10-15 11:41:17 +0200

    styling member list only country/ callsign and styling member list

--- assets/javascript/member-list.js
+++ assets/javascript/member-list.js
@@ -1,22 +1,23 @@
+let loadedData;
+
+
 async function init(){
     let JSON = './assets/JSON/member.json';
     let responseFromJSON = await fetch(JSON);
-    let loadedData = await responseFromJSON.json();
-    renderCountryList(loadedData);
-    // setTimeout(showWelcomeText, 1000);
-    console.log(loadedData);
+    loadedData = await responseFromJSON.json();
+    // renderCountryList();
+    setTimeout(renderCountryList, 500);
 }
 
-function renderCountryList(loadedData){ 
+function renderCountryList(){ 
     allMemberList.innerHTML ="";
     for (let i = 0; i < loadedData.length; i++) {
         const countryImage = loadedData[i]['flag'];
         const countryName = loadedData[i]['country'];
         const preFix = loadedData[i]['prefix'];
         allMemberList.innerHTML += `
-        
                                 <div class="country-card">
-                                    <div id="${countryName}" class="country-card-wrapper txt-center">
+                                    <div id="loadedData-${i}" class="country-card-wrapper txt-center">
                                         <h3>
                                             ${countryName}
                                         </h3>
@@ -28,7 +29,7 @@ function renderCountryList(loadedData){
                                     </div>
                                 </div>
                                 `;
-        console.log(countryImage, countryName );
+        console.log(countryImage, countryName,loadedData[i] );
     }
     
 }    
